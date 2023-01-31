16f7fa9d1755794ec682d025301355d33489dd78
Cesar Martinez <km.music92@gmail.com>
2023-01-31 17:19:11 -0500

    update: app.service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,18 +1,20 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
+import { Db } from 'mongodb';
+
 import config from './environments/config';
 
 @Injectable()
 export class AppService {
   constructor(
-    @Inject('TASKS') private tasks: any[],
+    //@Inject('MONGO') private mongoDB: Db,
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
-  test(): any {
-    const apiKey = this.configService.apiKey;
-    const dbName = this.configService.database.name;
-    const dbPort = this.configService.database.port;
-    return `api: ${apiKey} db: ${dbName} db_port: ${dbPort}`;
-    //return this.tasks;
+  async test() {
+    // const tasksCollection = await this.mongoDB.collection('tasks');
+    // console.log(tasksCollection);
+    // const tasks = await tasksCollection.find().toArray();
+    // return tasks;
+    return "hola";
   }
 }
